e5af43b24637d110dce7ee578326c3a7b3978ae4
k26dr <kedariyer26@gmail.com>
2014-09-15 19:01:11 +0530

    database details updated

--- database.php
+++ database.php
@@ -16,8 +16,8 @@ $capsule->addConnection(array(
     'driver'    => 'mysql',
     'host'      => 'localhost',
     'database'  => 'arogita',
-    'username'  => 'root',
-    'password'  => 'can',
+    'username'  => 'ayurvedaacademy',
+    'password'  => 'ayurevlearn123',
     'charset'   => 'utf8',
     'collation' => 'utf8_unicode_ci',
     'prefix'    => ''
